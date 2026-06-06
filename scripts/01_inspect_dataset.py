2b9e098563cfc103f4dd67203ae8520abf1e9af1
Linnan Zhu <zlnene0100@gmail.com>
2026-06-06 19:26:35 +0800

    Add dataset inspection script

---
+++ scripts/01_inspect_dataset.py
@@ -0,0 +1,11 @@
+"""
+Inspect OpenNeuro ds003592 metadata.
+
+Planned tasks:
+- Load participants.tsv
+- Check available age variables
+- Identify younger and older participants
+- Check available resting-state fMRI files
+"""
+
+# This script will be completed after downloading the dataset metadata.
