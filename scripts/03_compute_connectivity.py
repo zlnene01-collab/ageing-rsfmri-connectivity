9dc8129de4bca9c3d2d5a6845f0ca10ee174b707
Linnan Zhu <zlnene0100@gmail.com>
2026-06-06 19:28:23 +0800

    Add connectivity computation script

---
+++ scripts/03_compute_connectivity.py
@@ -0,0 +1,11 @@
+"""
+Compute functional connectivity matrices.
+
+Planned tasks:
+- Load ROI-level time series
+- Compute correlation matrix
+- Save subject-level connectivity matrix
+- Create connectivity heatmap
+"""
+
+# This script will be completed after ROI time series extraction is working.
